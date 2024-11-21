e943be45db9b8bf2f23eed5e4ffa62b398af9a66
Pham Duy Hoang <97928971+kng-duyhoang@users.noreply.github.com>
2024-11-21 10:29:02 +0700

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,6 +16,14 @@ const router = express.Router()
 // router.use('/v1/api/notification', require('./notification'))
 // router.use('/v1/api/upload', require('./upload'))
 // router.use('/v1/api', require('./access'))
+router.get('/check', (req, res, next) => {
+    res.status(200).json({
+        message: 'success',
+        data: {
+            name: "goldz"
+        }
+    })
+})
 router.get('/api/get', (req, res, next) => {
     res.status(200).json({
         message: 'success',
